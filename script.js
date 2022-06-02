8df5b95de3133d1d579e835e6070b18276ae0647
KAIQUE ALMEIDA SILVA <43797086+kaiqueealm@users.noreply.github.com>
2022-06-02 17:52:44 -0300

    atualização

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function verificar(){
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value) //number pra troca o teclado de letra por numero
         var genero = ''
-        var img = document.createElement('img')
+        var img = document.createElement('img') // para colcar uma imagen
         img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = 'Homem'
